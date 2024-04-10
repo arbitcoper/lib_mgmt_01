73ed71b8a3d388a14e7a3e7480bf54a5e7c1fdac
arbitcoper <145247219+arbitcoper@users.noreply.github.com>
2024-04-10 16:24:58 +0530

    Create main_file.c

---
+++ main_file.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	  clrscr();
+	
+	  int ch = 0;
+  	while (ch != ) {
+    		printf("...Formatting...");
+        printf("1. Admin \n");
+    printf("2. Librarian \n");
+    printf("3. Student \n");
+  }
+}
